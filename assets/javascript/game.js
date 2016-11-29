a89b79b776eec1ef27c83512e9a64277d1e1cf0c
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-11-28 19:50:25 -0500

    added initial images and layout. added very simple  click function to character images

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+	
+	// function construct_player(health_points, attack_power, counter_power) {
+	//     this.health = health_points;
+	//     this.attack = attack_power;
+	//     this.counter= counter_power;
+	// }
+
+	// var char_list = ["judy", "nick", "merry", "sloth"]
+
+	// alert("hi")
+	// for (c in char_list){
+	// 	var current_name = char_list[c]
+	// 	var current_name = new construct_player(5,3,4)
+	// }; 
+
+	// console.log(char_list)
+	// console.log(judy.health)
+
+
+
+
+	$(".char").on("click", function() {
+
+	   alert("yo")
+	});
+});
