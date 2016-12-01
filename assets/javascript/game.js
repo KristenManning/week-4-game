8eb6295bd39de69796686a66ed9f1230c230a8c0
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-01 16:40:30 -0500

    Fixed error that made it too difficult for player to win. Updated the instructions div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -57,9 +57,8 @@ $( document ).ready(function() {
 		player = character_list[$(clicked_img).data("charnum")]
 		$("#you").attr("src", clicked_img.src)
 		$(".instruct").text("Click on an enemy to select your opponent")
-		$(".char").addClass("enemies_border")
-		$(".char").css("opacity", "0.5")
-		$(clicked_img).removeClass("enemies_border")
+		// $(".char").css("opacity", "0.5")
+		
 		$(clicked_img).addClass("player_border")
 	}
 // Sets the opponent to be whichever character was clicked 
@@ -69,7 +68,62 @@ $( document ).ready(function() {
 			opponent = character_list[$(clicked_img).data("charnum")]
 			$("#opp").attr("src", clicked_img.src)
 			$(".instruct").text("Click the attack button to attack " + opponent.name)
+			$(".char").removeClass("enemies_border")
+			$(clicked_img).addClass("enemies_border")
+			$(".notes").html("")
 		}
+	}
+// Resets everything 
+	function restart(){
+		player = ""
+		opponent = ""
+		defeat_count = 0 
+			var judy = {
+		num: 0,
+		name: "Judy", 
+		base: Math.floor(Math.random()*15)+15, 
+		health: Math.floor(Math.random()*70)+90, 
+		attack: 0, 
+		counter: Math.floor(Math.random()*25)+20, 
+		}
+		$(".health0").html("HP = " + judy.health);
+
+		nick = {
+			num: 1,
+			name: "Nick",
+			base: Math.floor(Math.random()*15)+15, 
+			health: Math.floor(Math.random()*70)+90, 
+			attack: 0, 
+			counter: Math.floor(Math.random()*25)+20, 
+		}
+		$(".health1").html("HP = " + nick.health);
+
+		bellwether = {
+			num: 2, 
+			name: "Bellwether",
+			base: Math.floor(Math.random()*15)+15, 
+			health: Math.floor(Math.random()*70)+90, 
+			attack: 0, 
+			counter: Math.floor(Math.random()*25)+20, 
+		}
+		$(".health2").html("HP = " + bellwether.health);
+
+		flash = {
+			num: 3, 
+			name: "Flash",
+			base: Math.floor(Math.random()*15)+15, 
+			health: Math.floor(Math.random()*70)+90, 
+			attack: 0, 
+			counter: Math.floor(Math.random()*25)+20, 
+		}
+		$(".health3").html("HP = " + flash.health);
+
+		$(".instruct").text("Click on any character to select your player")
+		$("#opp").attr("src", "");
+		$("#you").attr("src", "");
+		$(".char").removeClass("enemies_border")
+		$(".char").removeClass("player_border")
+		$(".notes").html("")
 	}
 
 	$(".char").on("click", function() {
@@ -80,6 +134,8 @@ $( document ).ready(function() {
 		}
 	});
 
+	$("#restart_btn").on("click", restart); 
+
 	// When you click attack button 
 	$("#attack_btn").on("click", function() {
 
@@ -88,30 +144,41 @@ $( document ).ready(function() {
 		}else if(opponent == ""){
 			$(".instruct").text("Please select an opponent.")
 		}else{
-			// player's attack amount increases by base 
+			// Player attacks opponent. Player's attack amount increases by base.
 			player.attack += player.base; 
 			// opponent's health is decreased by player's attack amount 
 			opponent.health -= player.attack;
 			$(".health"+opponent.num).html("HP = " + opponent.health);
-			// player's health is decreased by opponents's attack amount [counter]
-			player.health -= opponent.counter; 
-			$(".health"+player.num).html("HP = " + player.health);
-
-			if (player.health <= 0){
-				$(".health"+player.num).html("DEFEATED");
-				$(".instruct").text("GAME OVER. You were defeated. Press restart to try again.")
-				$("#opp").attr("src", "");
-				$("#you").attr("src", "");
-			}else if(opponent.health <= 0){
-				$(".health"+opponent.num).html("DEFEATED");
-				$(clicked_img).css('opacity', '0.15') 
-				$("#opp").attr("src", "");
-				$(".instruct").text("You've defeated " + opponent.name +"! Click on another enemy");
+			// Check whether opponent was defeated 
+			if(opponent.health <= 0){
 				defeat_count ++ ;
-				opponent = ""; 
-				
-				
+				$(".health"+opponent.num).html("DEFEATED");
+				$("#opp").attr("src", "");
+
+				if (defeat_count == 3){
+					$(".instruct").text("You've defeated " + opponent.name +"! CONGRATULATIONS! You won. Click restart to play again.");
+					opponent = ""; 
+				}else{
+					$(".instruct").text("You've defeated " + opponent.name +"! Click on another enemy");
+					opponent = ""; 
+				}
+			
+			// If opponent was not defeated, continue. Opponent attacks player. 
+			}else{
+				// player's health is decreased by opponents's attack amount [counter]
+				player.health -= opponent.counter; 
+				$(".health"+player.num).html("HP = " + player.health);
+				$(".notes").html("<br> You attacked " + opponent.name + " for " + player.attack + " damage. " + "<br><br>" + opponent.name + " attacked you back for " + opponent.counter + " damage.");
+				// Check whether player was defeated. 
+				if (player.health <= 0){
+					$(".health"+player.num).html("DEFEATED");
+					$(".instruct").css("background-color", "black")
+					$(".instruct").text("You were defeated. GAME OVER. Click restart to try again.")
+					$("#opp").attr("src", "");
+					$("#you").attr("src", "");
+					opponent = ""; 
 					
+					}
 				}
 			}
 		});
