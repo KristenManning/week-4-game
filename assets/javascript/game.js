5b39a2aa24a233479f92087517a17b9bb1c239e9
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2016-12-02 23:54:54 -0500

    adding extra comments to javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
+
+	// GLOBAL VARIABLES 
 	
-	// function construct_player(health_points, attack_power, counter_power) {
-	//     this.health = health_points;
-	//     this.attack = attack_power;
-	//     this.counter= counter_power;
-	// }
+	// Create a variable for each character. **(There is probably a quicker way to do this with a function...)**
+	// Give each character a "base" which is a random number between 15 and 29. If that character is the player, base will be added to "attack" on each turn
+	// Give each character an initial health between 90 and 159 
+	// Give each character a "counter" which is the same as attack but for enemies. Does not increase with each turn 
+	// Set name as the character's name and num as the same number as data-charnum later on (makes it easy to relate this character to corresponding HTML elements)
 
 	var judy = {
 		num: 0,
@@ -52,83 +54,96 @@ $( document ).ready(function() {
 
 	character_list = [judy, nick, bellwether, flash];
 
-// Sets the users player to be whichever character was clicked 
-	function select_player(clicked_img){
-		player = character_list[$(clicked_img).data("charnum")]
-		$("#you").attr("src", clicked_img.src)
-		$(".instruct").text("Click on an enemy to select your opponent")
-		// $(".char").css("opacity", "0.5")
-		
-		$(clicked_img).addClass("player_border")
-	}
-// Sets the opponent to be whichever character was clicked 
-	function select_opponent(clicked_img){
-		// Opponent cannot be the same as player 
-		if (player.num != $(clicked_img).data("charnum")){
-			opponent = character_list[$(clicked_img).data("charnum")]
-			$("#opp").attr("src", clicked_img.src)
-			$(".instruct").text("Click the attack button to attack " + opponent.name + " or select a different opponent");
-			$(".char").removeClass("enemies_border")
-			$(clicked_img).addClass("enemies_border")
-			$(".notes").html("")
+// GAME PLAY FUNCTIONS 
+
+	// Sets the users player to be whichever character was clicked 
+		function select_player(clicked_img){
+			// In game logic 
+			player = character_list[$(clicked_img).data("charnum")]
+			// Visually 
+			$("#you").attr("src", clicked_img.src)
+			$(".instruct").text("Click on an enemy to select your opponent")
+			
+			$(clicked_img).addClass("player_border")
 		}
-	}
-// Resets everything 
-	function restart(){
-		player = ""
-		opponent = ""
-		defeat_count = 0 
-		judy = {
-			num: 0,
-			name: "Judy", 
-			base: Math.floor(Math.random()*15)+15, 
-			health: Math.floor(Math.random()*70)+90, 
-			attack: 0, 
-			counter: Math.floor(Math.random()*25)+20, 
+	// Sets the opponent to be whichever character was clicked 
+		function select_opponent(clicked_img){
+			// Opponent cannot be the same as player 
+			if (player.num != $(clicked_img).data("charnum")){
+				// In game logic 
+				opponent = character_list[$(clicked_img).data("charnum")]
+				// Visually 
+				$("#opp").attr("src", clicked_img.src)
+				$(".instruct").text("Click the attack button to attack " + opponent.name + " or select a different opponent");
+				// Remove border from previous enemy (if there was one)
+				$(".char").removeClass("enemies_border")
+				$(clicked_img).addClass("enemies_border")
+				// Reset 'notes' section from previous enemy (if there was one)
+				$(".notes").html("")
 			}
-		$(".health0").html("HP = " + judy.health);
-
-		nick = {
-			num: 1,
-			name: "Nick",
-			base: Math.floor(Math.random()*15)+15, 
-			health: Math.floor(Math.random()*70)+90, 
-			attack: 0, 
-			counter: Math.floor(Math.random()*25)+20, 
 		}
-		$(".health1").html("HP = " + nick.health);
+	// Resets everything 
+		function restart(){
+			player = ""
+			opponent = ""
+			defeat_count = 0 
+			// Reset each character 
+			judy = {
+				num: 0,
+				name: "Judy", 
+				base: Math.floor(Math.random()*15)+15, 
+				health: Math.floor(Math.random()*70)+90, 
+				attack: 0, 
+				counter: Math.floor(Math.random()*25)+20, 
+				}
+			$(".health0").html("HP = " + judy.health);
 
-		bellwether = {
-			num: 2, 
-			name: "Bellwether",
-			base: Math.floor(Math.random()*15)+15, 
-			health: Math.floor(Math.random()*70)+90, 
-			attack: 0, 
-			counter: Math.floor(Math.random()*25)+20, 
+			nick = {
+				num: 1,
+				name: "Nick",
+				base: Math.floor(Math.random()*15)+15, 
+				health: Math.floor(Math.random()*70)+90, 
+				attack: 0, 
+				counter: Math.floor(Math.random()*25)+20, 
+			}
+			$(".health1").html("HP = " + nick.health);
+
+			bellwether = {
+				num: 2, 
+				name: "Bellwether",
+				base: Math.floor(Math.random()*15)+15, 
+				health: Math.floor(Math.random()*70)+90, 
+				attack: 0, 
+				counter: Math.floor(Math.random()*25)+20, 
+			}
+			$(".health2").html("HP = " + bellwether.health);
+
+			flash = {
+				num: 3, 
+				name: "Flash",
+				base: Math.floor(Math.random()*15)+15, 
+				health: Math.floor(Math.random()*70)+90, 
+				attack: 0, 
+				counter: Math.floor(Math.random()*25)+20, 
+			}
+			$(".health3").html("HP = " + flash.health);
+
+			//Update 'character_list' so that select_opponent and select_player functions work as expected 
+			character_list = [judy, nick, bellwether, flash];
+
+			// Reset everything visually 
+			$(".instruct").text("Click on any character to select your player")
+			$("#opp").attr("src", "");
+			$("#you").attr("src", "");
+			$(".char").removeClass("enemies_border")
+			$(".char").removeClass("player_border")
+			$(".notes").html("")
+			$(".instruct").css("background-color", "#FF6505")
 		}
-		$(".health2").html("HP = " + bellwether.health);
 
-		flash = {
-			num: 3, 
-			name: "Flash",
-			base: Math.floor(Math.random()*15)+15, 
-			health: Math.floor(Math.random()*70)+90, 
-			attack: 0, 
-			counter: Math.floor(Math.random()*25)+20, 
-		}
-		$(".health3").html("HP = " + flash.health);
+// BUTTONS: 
 
-		character_list = [judy, nick, bellwether, flash];
-
-		$(".instruct").text("Click on any character to select your player")
-		$("#opp").attr("src", "");
-		$("#you").attr("src", "");
-		$(".char").removeClass("enemies_border")
-		$(".char").removeClass("player_border")
-		$(".notes").html("")
-		$(".instruct").css("background-color", "#FF6505")
-	}
-
+	// If a player has not been chosen yet, then any clicked character becomes player. Otherwise, becomes opponent 
 	$(".char").on("click", function() {
 		if (player ==""){
 			select_player(this)
@@ -137,15 +152,17 @@ $( document ).ready(function() {
 		}
 	});
 
+	// Restart button resets everything visually and in game 
 	$("#restart_btn").on("click", restart); 
 
 	// When you click attack button 
 	$("#attack_btn").on("click", function() {
-
+		// Check that opponent and player are chosen 
 		if ((player == "") && (opponent == "")){
 			$(".instruct").text("Please select a player and an opponent.")
 		}else if(opponent == ""){
 			$(".instruct").text("Please select an opponent.")
+
 		}else{
 			// Player attacks opponent. Player's attack amount increases by base.
 			player.attack += player.base; 
@@ -154,13 +171,15 @@ $( document ).ready(function() {
 			// opponent's health is decreased by player's attack amount 
 			opponent.health -= player.attack;
 			$(".health"+opponent.num).html("HP = " + opponent.health);
-			// Check whether opponent was defeated 
+
+			// Check whether opponent was defeated and how many opponents have been defeated so far. 
+			// If opponent was defeated, update game & visuals accordingly 
 			if(opponent.health <= 0){
 				defeat_count ++ ;
 				$(".health"+opponent.num).html("DEFEATED");
 				$("#opp").attr("src", "");
-
 				if (defeat_count == 3){
+					$(".instruct").css("background-color", "black")
 					$(".instruct").text("You've defeated " + opponent.name +"! CONGRATULATIONS! You won. Click restart to play again.");
 					opponent = ""; 
 				}else{
@@ -168,7 +187,7 @@ $( document ).ready(function() {
 					opponent = ""; 
 				}
 			
-			// If opponent was not defeated, continue. Opponent attacks player. 
+			// If opponent was not defeated, continue -- Opponent attacks player. 
 			}else{
 				// player's health is decreased by opponents's attack amount [counter]
 				player.health -= opponent.counter; 
@@ -187,8 +206,11 @@ $( document ).ready(function() {
 				}
 			}
 		});
+});
+
+
+
+
 		// $(".char").on("click", function() {
 		// 	player = character_list[$(this).data("charnum")]
 		// });		
-
-});
